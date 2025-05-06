7a561a7685063a6a1c80285e9358c2dfe3997cbd
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2025-05-06 09:54:19 +0200

    Update App.js

--- consult-park/src/App.js
+++ consult-park/src/App.js
@@ -12,7 +12,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/outsource" element={<Outsource />} />
-          
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
