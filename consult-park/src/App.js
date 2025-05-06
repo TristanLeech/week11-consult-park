db04717fb3c71738fa2dea55bb0c3b33337ddea8
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2025-05-06 17:10:09 +0200

    Update App.js

--- consult-park/src/App.js
+++ consult-park/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
