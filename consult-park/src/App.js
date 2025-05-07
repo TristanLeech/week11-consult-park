a38b5a42a7214fe7f76132b2b7067e784b76cb3e
MarioOjo <159897985+MarioOjo@users.noreply.github.com>
2025-05-07 13:29:12 +0200

    added more components

--- consult-park/src/App.js
+++ consult-park/src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Outsource from './components/Outsource';
+import Contact from './components/Contact';
 
 function App() {
   return (
