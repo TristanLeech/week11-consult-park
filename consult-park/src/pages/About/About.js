4566518a50f30847cbda37a1fddfef87bfb3148a
TristanLeech <241365@virtualwindow.co.za>
2025-05-08 10:53:51 +0200

    Div fixes

--- consult-park/src/pages/About/About.js
+++ consult-park/src/pages/About/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./About.css";
 import Team from "../../assets/Team.jpeg";
+import Vision from "../../assets/missionVision.jpeg";
 
 function About() {
   return (
@@ -18,6 +19,26 @@ function About() {
         </p></div>
         
       </div>
+
+      <div className="missVis">
+        <div className="visText">
+          <h2>Mission and Vision</h2>
+          <h1>Getting you where you need to be</h1>
+        <p><b>
+          We use cutting edge techniques to provide the best consulting services in the industry. Our mission is to empower businesses with innovative solutions that drive growth and success. We envision a future where our clients thrive in a competitive landscape, leveraging our expertise to achieve their goals.
+        </b></p></div>
+        <div  className="teamImage" ><img id="teamImage" src={Vision} width="600px" border-radius="16px"/></div>
+      </div>
+
+      <div className="values"></div>
+        <h2>Our Values</h2>
+        <h1>Integrity, Innovation, Excellence</h1>
+        <p>
+          At Consult Park, we believe in the power of integrity, innovation,
+          and excellence. These values guide our actions and decisions, ensuring
+          that we always put our clients first and deliver the highest quality
+          services.
+        </p>
     </div>
   );
 }
